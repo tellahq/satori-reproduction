8187d12ec0c0f2a47c10714489e31368e6a16b40
Ozan Şener <ozan@tella.tv>
2023-02-17 11:39:08 +0100

    Add satori example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import satori from "satori";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const svg = await satori(
+    <div
+      style={{
+        backgroundColor: "tomato",
+        width: "400px",
+        height: "400px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src="https://picsum.photos/200/300" width={200} height={300} />
+    </div>,
+    {
+      width: 600,
+      height: 400,
+      fonts: [],
+    }
+  );
+
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.status(200).send(svg);
 }
